722a9626112ef08998837160395bfa7e24606c2b
Raymundo Daniel Ramírez Ramos <ray.ramirez@unikino.edu.mx>
2023-12-11 23:27:11 -0700

    4ta forma de hacer uso de un Snackbar

--- app/src/main/java/com/example/snackbars/MainActivity.java
+++ app/src/main/java/com/example/snackbars/MainActivity.java
@@ -1,16 +1,19 @@
 package com.example.snackbars;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.coordinatorlayout.widget.CoordinatorLayout;
 
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import com.google.android.material.snackbar.BaseTransientBottomBar;
 import com.google.android.material.snackbar.Snackbar;
 
 public class MainActivity extends AppCompatActivity {
 
     private Button mSnackbarBtn;
+    private CoordinatorLayout mSnackbarLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,13 +21,16 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         mSnackbarBtn = findViewById(R.id.snack_btn);
+        mSnackbarLayout = findViewById(R.id.snackbar_layout);
 
         mSnackbarBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Snackbar snackbar = Snackbar.make(v, "Snackbar 3", Snackbar.LENGTH_LONG);
+                Snackbar snackbar = Snackbar.make(v, "Snackbar 4", Snackbar.LENGTH_LONG);
                 snackbar.setDuration(10000);
+                snackbar.setAnimationMode(BaseTransientBottomBar.ANIMATION_MODE_SLIDE);
                 snackbar.setAction("OK", new View.OnClickListener() {
+                    @Override
                     public void onClick(View v) {
                         //
                     }
